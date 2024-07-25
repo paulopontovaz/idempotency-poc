39f1b03b1483cf8208db8dad2d2f0b132825a5ae
Paulo Vaz <paulopontovaz@gmail.com>
2024-07-25 20:03:12 +0200

    feat(routing): add base path to main router

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -2,10 +2,10 @@ import { personsRouter } from "@/server/api/routes";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-const server = new Hono();
+const server = new Hono().basePath("/api");
 
 if (process.env.NODE_ENV === "dev") {
-	server.use("/api/*", cors());
+    server.use("/api/*", cors());
 }
 
 server.route("/persons", personsRouter);
