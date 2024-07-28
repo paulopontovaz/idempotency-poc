2a7534813382709571a7369c301f1f3bb4e7046d
Paulo Vaz <paulopontovaz@gmail.com>
2024-07-28 16:47:29 +0200

    feat(client): use new idempotency hook when creating new entities

--- client/src/services/_api/useAddPerson.ts
+++ client/src/services/_api/useAddPerson.ts
@@ -1,26 +1,41 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type {
     Person,
     PersonInsert,
 } from "../../../../server/db/models/personsModel";
 import { PERSONS_API_URL, api } from "./_common";
+import { getPersonsQueryKey } from "./useGetAllPersons";
+import { useIdempotencyKey } from "./useIdempotencyKey";
 
-type AddPersonResponse = {
-    person: Person;
-};
+type AddPersonResponse = { person: Person };
 
-export const addPersonRequest = async (
-    person: PersonInsert,
-): Promise<AddPersonResponse> =>
-    (await api
-        .post(PERSONS_API_URL, {
-            json: person,
-        })
-        .json()) as AddPersonResponse;
+export const addPersonRequest =
+    (getIdempotencyKeyFunction: (body: unknown) => string) =>
+    async (person: PersonInsert): Promise<AddPersonResponse> => {
+        const idempotencyKey = getIdempotencyKeyFunction(person);
+
+        return (await api
+            .post(PERSONS_API_URL, {
+                json: person,
+                headers: {
+                    "Idempotency-Key": idempotencyKey,
+                },
+            })
+            .json()) as AddPersonResponse;
+    };
 
 export const useAddPerson = () => {
+    const { getIdempotencyKey } = useIdempotencyKey();
+    const queryClient = useQueryClient();
+
     const { mutateAsync: addPerson } = useMutation({
-        mutationFn: addPersonRequest,
+        mutationFn: addPersonRequest(getIdempotencyKey),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: getPersonsQueryKey(),
+                refetchType: "active",
+            });
+        },
     });
 
     return { addPerson };
