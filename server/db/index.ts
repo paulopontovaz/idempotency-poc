75e23041275076c0e087d53489107ec7ed2f07d7
Paulo Vaz <paulopontovaz@gmail.com>
2024-08-01 17:46:12 +0200

    feat(global): use env values in more places in the code

--- server/db/index.ts
+++ server/db/index.ts
@@ -3,12 +3,12 @@ import { Pool } from "pg";
 import * as schema from "./models";
 
 const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "postgres",
-    database: "idempotency_poc",
-    ssl: false,
+    host: process.env.DB_HOST ?? "localhost",
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+    user: process.env.DB_USER ?? "postgres",
+    password: process.env.DB_PASS ?? "postgres",
+    database: process.env.DB_NAME ?? "idempotency_poc",
+    ssl: process.env.DRIZZLE_SSL !== "false",
 });
 
 export const db = drizzle(pool, { schema });
--- server/redis/redisClient.ts
+++ server/redis/redisClient.ts
@@ -1,5 +1,9 @@
 import { createClient } from "redis";
 
-export const redisClient = await createClient().connect();
+export const redisClient = await createClient({
+    url: `redis://${process.env.REDIS_HOST ?? "localhost"}:6379}/0`,
+})
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
 
 process.on("exit", () => redisClient.quit());
