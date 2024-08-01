65d2c4a5c6989d9deae59db510a7d36acdca39bf
Paulo Vaz <paulopontovaz@gmail.com>
2024-08-01 18:03:15 +0200

    fix(global): running setup

--- server/redis/redisClient.ts
+++ server/redis/redisClient.ts
@@ -1,8 +1,12 @@
 import { createClient } from "redis";
 
-export const redisClient = await createClient({
-    url: `redis://${process.env.REDIS_HOST ?? "localhost"}:6379}/0`,
-})
+export const redisClient = await createClient(
+    process.env.REDIS_HOST
+        ? {
+              url: `redis://${process.env.REDIS_HOST}:6379/0`,
+          }
+        : undefined,
+)
     .on("error", (err) => console.log("Redis Client Error", err))
     .connect();
 
